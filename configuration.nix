39d24374039ff60231bf56543eeccb7ac58171c7
MarcFontaine <MarcFontaine@users.noreply.github.com>
2020-04-26 16:21:07 +0200

    new README.md

--- configuration.nix
+++ configuration.nix
@@ -5,16 +5,15 @@
 with lib;
 
 {
-  imports = [ <nixpkgs/nixos/modules/installer/netboot/netboot-minimal.nix> ./autoreboot.nix ./kexec.nix ./justdoit.nix ];
+  imports = [ <nixpkgs/nixos/modules/installer/netboot/netboot-minimal.nix> ./kexec.nix ./justdoit.nix ];
 
-  boot.supportedFilesystems = [ "zfs" ];
+  boot.supportedFilesystems = [ ];
   boot.loader.grub.enable = false;
   boot.kernelParams = [
-    "console=ttyS0,115200"          # allows certain forms of remote access, if the hardware is setup right
-    "panic=30" "boot.panic_on_fail" # reboot the machine upon fatal boot issues
+    "panic=90" "boot.panic_on_fail" # reboot the machine upon fatal boot issues
   ];
   systemd.services.sshd.wantedBy = mkForce [ "multi-user.target" ];
-  networking.hostName = "kexec";
+  networking.hostName = "nixinstall-kexec";
   # example way to embed an ssh pubkey into the tar
-  # users.users.root.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC34wZQFEOGkA5b0Z6maE3aKy/ix1MiK1D0Qmg4E9skAA57yKtWYzjA23r5OCF4Nhlj1CuYd6P1sEI/fMnxf+KkqqgW3ZoZ0+pQu4Bd8Ymi3OkkQX9kiq2coD3AFI6JytC6uBi6FaZQT5fG59DbXhxO5YpZlym8ps1obyCBX0hyKntD18RgHNaNM+jkQOhQ5OoxKsBEobxQOEdjIowl2QeEHb99n45sFr53NFqk3UCz0Y7ZMf1hSFQPuuEC/wExzBBJ1Wl7E1LlNA4p9O3qJUSadGZS4e5nSLqMnbQWv2icQS/7J8IwY0M8r1MsL8mdnlXHUofPlG1r4mtovQ2myzOx clever@nixos" ];
+  users.users.root.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDVGGe2Y2+zrzCa3BiFVaCH9RzoMmsBHR+/xCL2eBanYkukLz8V6a9FsjYhtirlwXMojcxWQSHif5YKw1doAc6lMYUxkEU6IgNIN/hAzPOR9C6c0EktFDjiMMJ0C0QlFxumcPHiAMIltosF0Lkcr0jG+0KLt1UqLP48Ib1Zp3QLSYf9DziAoG0OUTx8yjq3kBvfpFboQPIdqhoc5bvKwIRhZoKDDKmdZajtN0Fq0CWeBV9HYyW7dWrqQh/uUTbP4PX3bAlid87xkJpJlwafZVdKb48Ktcic6AcByy5v/6bOMTTcpxWowQyUhRhdUdlCLHhx1e9ojwkoHz6jX3AsB/4uPu2skb9u3vGa4HvRrKOvZgVAFzVlHOJHxRjG9qHXom7vOuEqDKpX+tpfpQ+nu+QLSU+Js+bipCjBipVNVdM0WDgP1wVvMlPUX4zGKIszLDZH9iMDek3Clu3G6odygVlViJWOJ1q+VItFGUqYR2KpA5OuC4Px/dle9bEHo80JunvuGc8XXvgGAWGCC8jQqUsXwzXjmWHifxevB1p2c2zFSyWgvhUc+0ftWaYwZa+uAMg21Fb+VpFQSL7/Vl8t1ZPmipKgz0o0SQ9JjS1s9pvb2jlPb8N7qeX2O6rQxsqdmkhOKtqbB1QvFjzsj8QhUMfLdw4XmSl/moLyi5bq+Oo7Rw== cardno:000611300226"];
 }
