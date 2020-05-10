2bc64b7ed491d105a76efc20bf73091952095c87
MarcFontaine <MarcFontaine@users.noreply.github.com>
2020-05-10 20:08:41 +0200

    template for setting the channel

--- installer/set-channels.sh
+++ installer/set-channels.sh
@@ -1,2 +1,5 @@
 # This script is called to prepare the channels
 # nix-channel --update
+# nix-channel --add https://nixos.org/channels/nixos-20.03 nixos
+# nix-channel --add https://nixos.org/channels/nixos-20.03 nixpkgs
+# nix-channel --update
