7482ca5aa25f6994a125cfcbb09f1e91c6253bc5
MarcFontaine <MarcFontaine@users.noreply.github.com>
2020-05-10 22:11:35 +0200

    try to fix start order

--- utils/nix.nix
+++ utils/nix.nix
@@ -9,13 +9,17 @@ rec {
 installer-files = (lib.cleanSourceWith {name = "installer-files"; src= ../installer;}).outPath ;
 
 auto-install-service = {
-    wantedBy = [ "multi-user.target" ]; 
-    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target"];
+    before = [ "getty-pre.target" "getty.target"];
+
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+
     description = "Auto install NIXOS on HD";
     serviceConfig = {
       StandardOutput = "tty";
       StandardError  = "tty";
-      # run in an environment simlilar to a regular login
+      # run in an environment similar to a regular login
       ExecStart = ''/run/wrappers/bin/su -l root -c ${auto-install}/bin/auto-install 2>&1'';
     };
 };
