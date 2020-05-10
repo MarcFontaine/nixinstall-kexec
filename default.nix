3cb2946210eea75f06eb6b5089da9935bf16548b
MarcFontaine <MarcFontaine@users.noreply.github.com>
2020-05-10 20:08:11 +0200

    pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,7 +1,13 @@
 {
 }:
 let
+  nixsrc = builtins.fetchTarball {
+    name = "nixpkgs-src";
+    url  = "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+  };
+
   configuration = ./tarball/configuration.nix;
-  nixos = import <nixpkgs/nixos> {inherit configuration;};
+  nixos = import "${nixsrc}/nixos" {inherit configuration;};
 in
 nixos.config.system.build.kexec_tarball
