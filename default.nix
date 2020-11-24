4b96be7066e34260be60febfda96414364811195
MarcFontaine <MarcFontaine@users.noreply.github.com>
2020-11-24 11:23:37 +0100

    update to 20.09

--- default.nix
+++ default.nix
@@ -3,8 +3,8 @@
 let
   nixsrc = builtins.fetchTarball {
     name = "nixpkgs-src";
-    url  = "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz";
-    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+    url  = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
+    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
   };
 
   configuration = ./tarball/configuration.nix;
--- installer/set-channels.sh
+++ installer/set-channels.sh
@@ -1,4 +1,4 @@
 # This script is called to prepare the channels
-nix-channel --add https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz nixos
-nix-channel --add https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz nixpkgs
+nix-channel --add https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz nixos
+nix-channel --add https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz nixpkgs
 nix-channel --update
